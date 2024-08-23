80fdb56306822bb936fe70d19adc37117dda1631
SANTOSH SAROJ <118922022+santoshsaroj0032@users.noreply.github.com>
2024-08-23 13:22:40 +0530

    Update Todo.java

--- src/main/java/com/llutsefer/springboot/todoWebApp/todo/Todo.java
+++ src/main/java/com/llutsefer/springboot/todoWebApp/todo/Todo.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size; 
-
+ 
 import java.time.LocalDate;
 
  @Entity 
